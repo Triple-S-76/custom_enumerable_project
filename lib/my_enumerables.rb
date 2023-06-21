40d8065ea3fb67dbd167f1547997569acc370263
TripleS <triple-s-76@outlook.com>
2023-06-21 11:18:55 -0700

    Finished attempt 2

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,92 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    count = 0
+    while count < length
+      p yield(self[count], count)
+      p 'break'
+      count += 1
+    end
+    self
+  end
+
+  def my_all?
+    count = 0
+    while count < length
+      return false if (yield self[count]) == false
+
+      count += 1
+    end
+    true
+  end
+
+  def my_any?
+    count = 0
+    while count < length
+      return true if (yield self[count]) == true
+
+      count += 1
+    end
+    false
+  end
+
+  def my_count
+    return length if block_given? == false
+
+    count = 0
+    count_result = 0
+
+    while count < length
+      result = yield self[count]
+      count_result += 1 if result == true
+      count += 1
+    end
+    count_result
+  end
+
+  def my_inject(arg)
+    count = 0
+    result = arg
+    while count < length
+      result = yield(result, self[count])
+      count += 1
+    end
+    result
+  end
+
+  def my_map
+    count = 0
+    final_result = []
+    while count < length
+      result = yield self[count]
+      final_result << result
+      count += 1
+    end
+    final_result
+  end
+
+  def my_none?
+    count = 0
+    while count < length
+      result = yield self[count]
+      return false if result == true
+
+      count += 1
+    end
+    true
+  end
+
+  def my_select
+    count = 0
+    final_result = []
+    while count < length
+      result = yield self[count]
+      final_result << self[count] if result == true
+      count += 1
+    end
+    final_result
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +95,13 @@ end
 # to this method
 class Array
   # Define my_each here
+
+  def my_each
+    count = 0
+    while count < length
+      yield self[count]
+      count += 1
+    end
+    self
+  end
 end
